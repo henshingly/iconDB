2c404c0aed7d011ef88fd06322d8a2e0ab685910
Henshingly <dietmar_kersting@web.de>
2026-06-07 16:59:30 +0200

    Add HTTP Basic Auth protection for adminer directory during setup

--- iconDB/setup.php
+++ iconDB/setup.php
@@ -16,9 +16,14 @@ $lang = [
         'SETUP_DB_USER' => 'DB-User',
         'SETUP_DB_PASS' => 'DB-Passwort',
         'SETUP_DB_NAME' => 'DB-Name',
+        'SETUP_ADMIN_USER' => 'Admin-Benutzername (für Adminbereich)',
+        'SETUP_ADMIN_PASS' => 'Admin-Passwort',
+        'SETUP_ADMIN_PASS_CONFIRM' => 'Admin-Passwort bestätigen',
         'SETUP_SUCCESS' => 'Installation erfolgreich',
         'NEXT'          => 'weiter',
         'GO_ADMIN'      => 'Zum Adminbereich',
+        'ERROR_PASS_MISMATCH' => 'Passwörter stimmen nicht überein!',
+        'ERROR_EMPTY_FIELDS' => 'Bitte füllen Sie alle Felder aus!',
     ],
     'en' => [
         'SETUP_HEAD'    => 'Installation for LMO-IconDataBase',
@@ -26,9 +31,14 @@ $lang = [
         'SETUP_DB_USER' => 'DB-User',
         'SETUP_DB_PASS' => 'DB-Password',
         'SETUP_DB_NAME' => 'DB-Name',
+        'SETUP_ADMIN_USER' => 'Admin username (for admin area)',
+        'SETUP_ADMIN_PASS' => 'Admin password',
+        'SETUP_ADMIN_PASS_CONFIRM' => 'Confirm admin password',
         'SETUP_SUCCESS' => 'Installation successful',
         'NEXT'          => 'next',
         'GO_ADMIN'      => 'Go to admin area',
+        'ERROR_PASS_MISMATCH' => 'Passwords do not match!',
+        'ERROR_EMPTY_FIELDS' => 'Please fill in all fields!',
     ],
 ];
 
@@ -72,6 +82,16 @@ if ($step === '0') {
       <div class="row p-1">
         <div class="col-3"><?php echo $lang[$userlang]['SETUP_DB_NAME'] ?></div><div class="col-2"><input class="form-control" type="text" name="dbname"></div>
       </div>
+      <hr>
+      <div class="row p-1">
+        <div class="col-3"><?php echo $lang[$userlang]['SETUP_ADMIN_USER'] ?></div><div class="col-2"><input class="form-control" type="text" name="adminuser" required></div>
+      </div>
+      <div class="row p-1">
+        <div class="col-3"><?php echo $lang[$userlang]['SETUP_ADMIN_PASS'] ?></div><div class="col-2"><input class="form-control" type="password" name="adminpass" required></div>
+      </div>
+      <div class="row p-1">
+        <div class="col-3"><?php echo $lang[$userlang]['SETUP_ADMIN_PASS_CONFIRM'] ?></div><div class="col-2"><input class="form-control" type="password" name="adminpass_confirm" required></div>
+      </div>
       <div class="row">
         <div class="col"><input class="btn btn-primary btn-sm" type="submit" value="<?php echo $lang[$userlang]['NEXT'] ?>"></div>
       </div>
@@ -89,6 +109,22 @@ if ($step === '1') {
     $dbuser = $_POST['dbuser'] ?? '';
     $dbpass = $_POST['dbpass'] ?? '';
     $dbname = $_POST['dbname'] ?? '';
+    $adminuser = $_POST['adminuser'] ?? '';
+    $adminpass = $_POST['adminpass'] ?? '';
+    $adminpass_confirm = $_POST['adminpass_confirm'] ?? '';
+
+    // Validierung
+    if (empty($adminuser) || empty($adminpass)) {
+        echo '<div class="alert alert-danger">' . $lang[$userlang]['ERROR_EMPTY_FIELDS'] . '</div>';
+        echo '<a href="' . htmlspecialchars($_SERVER['PHP_SELF']) . '">' . $lang[$userlang]['NEXT'] . '</a>';
+        exit;
+    }
+
+    if ($adminpass !== $adminpass_confirm) {
+        echo '<div class="alert alert-danger">' . $lang[$userlang]['ERROR_PASS_MISMATCH'] . '</div>';
+        echo '<a href="' . htmlspecialchars($_SERVER['PHP_SELF']) . '">' . $lang[$userlang]['NEXT'] . '</a>';
+        exit;
+    }
 
     $config = "<?php\n"
         . "//database settings\n"
@@ -121,6 +157,38 @@ if ($step === '1') {
         exit;
     }
     fclose($temp);
+
+    // Create .htaccess for adminer directory with proper formatting
+    $htaccess_content = "# Icon-DB Admin Area Protection\n"
+        . "AuthType Basic\n"
+        . "AuthName \"Icon-DB\"\n"
+        . "AuthUserFile " . dirname(__FILE__) . "/adminer/.htpasswd\n"
+        . "require valid-user";
+
+    $htaccess_file = fopen('adminer/.htaccess', 'w');
+    if (!fwrite($htaccess_file, $htaccess_content)) {
+        echo 'ERROR!! Cannot write .htaccess file - CHMOD adminer directory to 755';
+        fclose($htaccess_file);
+        exit;
+    }
+    fclose($htaccess_file);
+
+    // Create .htpasswd file with SHA1 hashing (Apache compatible)
+    // Using SHA1 like in your working example
+    $sha1_hash = '{SHA}' . base64_encode(sha1($adminpass, true));
+    $htpasswd_hash = $adminuser . ':' . $sha1_hash;
+
+    $htpasswd_file = fopen('adminer/.htpasswd', 'w');
+    if (!fwrite($htpasswd_file, $htpasswd_hash)) {
+        echo 'ERROR!! Cannot write .htpasswd file - CHMOD adminer directory to 755';
+        fclose($htpasswd_file);
+        exit;
+    }
+    fclose($htpasswd_file);
+
+    // Set proper file permissions
+    chmod('adminer/.htaccess', 0644);
+    chmod('adminer/.htpasswd', 0644);
 
     require_once('cfg.php');
     require_once('db_connect.php');
